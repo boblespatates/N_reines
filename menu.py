46b3ee0a788c7ce95ea8309a0960b1fd1e06b99f
boblespatates <noelzablocki@yahoo.fr>
2016-09-29 19:16:21 +0200

    creation de classe et menu (menu est vraiment pas un menu)

---
+++ menu.py
@@ -0,0 +1,16 @@
+# -*- codinf: utf-8 -*-
+
+from tinker import *
+from
+Largeur_Fenetre=500
+Longueur_Fenetre=500
+
+fenetre = Tk()
+
+canvas = Canvas(fenetre, width=Longueur_Fenetre, height=Largeur)
+
+for line in range(n):
+	for colonne in range(n):
+		
+
+fenetre.mainloop()
