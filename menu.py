82a5542609599cb3ae0665d047d8caffa255bbf4
boblespatates <noelzablocki@yahoo.fr>
2016-10-23 12:10:10 +0200

    ajout de la classe menu

--- menu.py
+++ menu.py
@@ -1,16 +1,47 @@
-# -*- codinf: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from tinker import *
-from
-Largeur_Fenetre=500
-Longueur_Fenetre=500
+import numpy as np
+from Tkinter import *
 
-fenetre = Tk()
+class Menu(Frame):
+	''' Classe definissant un menu
+	qui va demander le nombre de dames,
+	et la technique de resolution.'''
+	
+	def __init__(self,fenetre):
+		''' Constructeur de la classe 
+		La fenetre menu'''
 
-canvas = Canvas(fenetre, width=Longueur_Fenetre, height=Largeur)
-
-for line in range(n):
-	for colonne in range(n):
+		Frame.__init__(self,fenetre, width=768, height=576)
+		self.pack(fill=BOTH)
 		
+		# Spinbox pour N_REINES
+		self.spinbox1 = Spinbox(self, from_=8, to=50, textvariable=Number)
+		self.spinbox1.pack()
+		
+		# Radio boutton pour ALGORITHME
+		for algo in ['Arbre Binaire', 'Brute Force']:
+			rb = Radiobutton(self, text=algo, value=algo, variable=Algorithme)
+			rb.pack()
 
-fenetre.mainloop()
+		# Fermer la fenetre
+		self.bouton_valider = Button(self, text="Valider", command=self.valider)
+		self.bouton_valider.pack()		
+
+		
+	def valider(self):
+		''' Recupere les donnees et ferme la fenetre '''
+
+		self.ALGORITHME = Algorithme.get()
+		self.N_REINES = Number.get()
+		print(self.N_REINES)
+		print(self.ALGORITHME)
+		self.destroy()
+fenetre = Tk()
+Number = IntVar()
+Algorithme = StringVar()
+a1 = Menu(fenetre)
+a1.mainloop()
+print(a1.N_REINES)
+print(a1.ALGORITHME)
