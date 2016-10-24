f9f3cac39b8bb535eaf397d87366ae8fb1188d79
boblespatates <noelzablocki@yahoo.fr>
2016-10-24 20:54:59 +0200

    correction de menu

--- menu.py
+++ menu.py
@@ -12,23 +12,55 @@ class Menu(Frame):
 		''' Constructeur de la classe menu '''
 
 		# Initialisation de la fentre
-		Frame.__init__(self,fenetre, width=768, height=576)
+		Frame.__init__(self,fenetre)
 		self.pack(fill=BOTH)
-		
+		self.initialize()
+
+
+	def initialize(self):
+		''' Place les widgets dans la fenetre '''
+
 		# Spinbox pour N_REINES
-		self.spinbox1 = Spinbox(self, from_=8, to=50, textvariable=Number)
-		self.spinbox1.pack()
+		self.spinbox()
+			
+		# Pour la lisibilite du menu
+		label_espace = Label(self, text=' ')
+                label_espace.pack()
 		
 		# Radios bouttons pour ALGORITHME
-		for algo in ['Arbre Binaire', 'Brute Force']:
-			rb = Radiobutton(self, text=algo, value=algo, variable=Algorithme)
-			rb.pack()
-
-		# Fermer la fenetre
-		self.bouton_valider = Button(self, text="Valider", command=self.valider)
-		self.bouton_valider.pack()		
-
+		self.radio_boutton()
 		
+		# Fermer la fenetre
+		self.boutton()
+	
+	def spinbox(self):
+		''' Cree une spinbox pour
+		le nombre de reines '''
+		
+		label1 = Label(self, text='Choix de N', font=('Arial 13'))
+                label1.pack()
+                self.spinbox1 = Spinbox(self, from_=4, to=50, textvariable=Number)
+                self.spinbox1.pack()
+		
+	def radio_boutton(self):
+		''' Cree des radiobouttons
+		pour le choix de l'algo '''
+
+		label2 = Label(self, text='Choix de l algorithme', font=('Arial 13'))
+                label2.pack()
+                for algo in ['Force Brute', 'Force Brute plus',
+                 'Parcours en profondeur', 'Parcours en largeur']:
+                        rb = Radiobutton(self, text=algo, value=algo, variable=Algorithme)
+                        rb.pack(side=LEFT)
+	
+	def boutton(self):
+		''' Cree un boutton pour
+		fermer la fenetre '''
+
+		self.bouton_valider = Button(self, text="Valider", command=self.valider)
+                self.bouton_valider.pack(side=BOTTOM)	
+
+	
 	def valider(self):
 		''' Recupere les donnees et ferme la fenetre '''
 
