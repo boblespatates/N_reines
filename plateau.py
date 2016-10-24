b32432a228007792db99515ec794a6b0699666c5
boblespatates <noelzablocki@yahoo.fr>
2016-10-24 21:42:36 +0200

    implementation de la grille

--- plateau.py
+++ plateau.py
@@ -13,16 +13,21 @@ class Plateau(Frame):
 		# Initialisation de la fentre
 		Frame.__init__(self,fenetre)
 		self.pack(fill=BOTH)
-		
+		self.initialize()
+	
+	def initialize(self):
+			
 		# La grille 
-		canvas = Canvas(self, width=(550+(N_REINES-8)*50), height=(550+(N_REINES-8)*50))
+		canvas = Canvas(self, width=(1+N_REINES*50), height=(1+N_REINES*50))
 		DIM_CASE = 50
-		x0,y0 = 9,9
+		x0,y0 = 1,1
 
 		for i in range(N_REINES+1):
+			print(x0+ DIM_CASE*i)
+			print(y0 + N_REINES*DIM_CASE)
 			canvas.create_line(x0+DIM_CASE*i, y0,x0+DIM_CASE*i,y0 + N_REINES*DIM_CASE)
         		canvas.create_line(x0, y0+DIM_CASE*i,x0+N_REINES*DIM_CASE ,y0+DIM_CASE*i)
-						
+		canvas.pack()				
 		
 fenetre = Tk()
 N_REINES = 8
