bd6145b3bc123214710f3f0567caffd77ca613f0
Mace De Gastines Etienne <emacedegastines@asi-testing>
2016-11-04 14:17:42 +0100

    liste positive renvoyée

--- Fonctions.py
+++ Fonctions.py
@@ -9,69 +9,49 @@ import numpy as np
 
 
 
-def enregistre( listePermutation, n ):
-    '''Enregistre la solution dans un fichier texte
-    - listePermutation : permutation à afficher
-    - n : taille de l'échiquier '''
-    '''# pour l'instant, l'affiche
-    G = np.zeros( n )
-    # copie la solution
-    for i in range( n ):
-        G[i] = abs( listePermutation[i] )
-    #affiche la solution (ascii)
-    B=""
-    for i in range( n - 1 ):
-        B += "██"
-    for i in range(n):
-        p = 2 * int( G[i] )
-        print( B[:p] + "☺" + B[p:] )
-    print( "*************************************************" )'''
-
-
     
-def test( listePermutation, n ):
+def test( permutation, n ):
     ''' vérifie que la permutation est solution
-    - listePermutation : permutation à vérifier
-    - n : taille de l'échiquier
-    - i : ligne à vérifier '''
+    - permutation : permutation à vérifier
+    - n : taille de l'échiquier'''
     for i in range( n - 1 ):
         for j in range( i + 1, n ):
-            if  j != i and abs( j - i ) == abs( abs( listePermutation[j] )-abs( listePermutation[i] ) ):
+            if  j != i and abs( j - i ) == abs( abs( permutation[j] )-abs( permutation[i] ) ):
                 return False
     return True
 
 
     
-def test2( listePermutation, n, i ): # la fonction marche aussi si on a pas une permutation
+def test2( permutation, n, i ): 
     ''' teste les conflits d'une permutation sur la i-ème ligne 
-    - listePermutation : permutation à vérifier
+    - permutation : permutation à vérifier
     - n : taille de l'échiquier
     - i : ligne à vérifier '''
     for j in range( n ):
-        if  j != i and ( listePermutation[j] == listePermutation[i] or abs( j - i ) == abs( abs( listePermutation[j] )-abs( listePermutation[i] ) ) ):
+        if  j != i and ( permutation[j] == permutation[i] or abs( j - i ) == abs( abs( permutation[j] )-abs( permutation[i] ) ) ):
             return False
     return True
 
 
 
-def parcoursEnProfondeurRec(listePermutation, i, n):
+def parcoursEnProfondeurRec(permutation, i, n):
     ''' fonction récursive pour le parcours en profondeur
-    - listePermutation : solution partielle étudiée
+    - permutation : solution partielle étudiée
     - i : nombre de ligne déjà remplies à l'entrée de la fonction
     - n : taille de l'échiquier
     - compteur : compte le nombre de solutions '''
     for j in range( n ):
-        listePermutation[i] = j
-        if not test2( listePermutation, i + 1, i ): 
+        permutation[i] = j
+        if not test2( permutation, i + 1, i ): 
             #si oui, déplace la reine vers la droite           
             continue
         #sinon, passe à la ligne suivante
         else :
             if i < n - 1:
-                parcoursEnProfondeurRec( listePermutation, i + 1, n)
+                parcoursEnProfondeurRec( permutation, i + 1, n)
             #enregistre la solution car on a parcouru toute les lignes
             else :
-                listeSolution.append(listePermutation)
+                listeSolution.append(permutation)
     #si une reine ne peut pas se placer sur une ligne, revient à la ligne précédente
     return listeSolution
     
@@ -80,8 +60,8 @@ def parcoursEnProfondeurRec(listePermutation, i, n):
 def parcoursEnProfondeur( n ):
     ''' parcourt le graphe en profondeur pour chercher les solutions
     - n : taille de l'échiquier '''
-    listePermutation = np.zeros( n )
-    return( parcoursEnProfondeurRec( listePermutation, 0, n) )
+    permutation = np.zeros( n )
+    return( parcoursEnProfondeurRec( permutation, 0, n) )
 
     
     
@@ -115,55 +95,56 @@ def parcoursEnLargeur(n):
     
         
     
-def nextPermutationSJT(listePermutation, n):
+def nextPermutationSJT(permutation, n):
     ''' pour une permutation donnée, calcule la permutation suivante selon l'algorithme de Steinhaus–Johnson–Trotter.
     Il faut que la direction soit précisée (nombre positif: direction vers la gauche, nombre négatif: direction vers la droite) 
-    - listePermutation : permutation actuelle
+    - permutation : permutation actuelle
     - n : taille de l'échiquier '''
     #cherche le plus grand élément, 
     for j in range(n-1, -1, -1): 
         for i in range(n):
-            if listePermutation[i] == j or listePermutation[i] == -j:
+            if permutation[i] == j or permutation[i] == -j:
                 p = i
                 break
         #vérifie la possibilité d'un décalage à gauche
-        if listePermutation[p] > 0 and p > 0 and abs( listePermutation[p - 1] ) < abs( listePermutation[p] ):
-            ( listePermutation[p], listePermutation[p - 1] ) = ( listePermutation[p - 1], listePermutation[p] )
+        if permutation[p] > 0 and p > 0 and abs( permutation[p - 1] ) < abs( permutation[p] ):
+            ( permutation[p], permutation[p - 1] ) = ( permutation[p - 1], permutation[p] )
             return ( True, p , p - 1 )
         #vérifie la possibilité d'un décalage à droite        
-        if listePermutation[p] < 0 and p < n - 1 and abs( listePermutation[p + 1] ) < abs( listePermutation[p] ):
-            ( listePermutation[p], listePermutation[p + 1] ) = ( listePermutation[p + 1], listePermutation[p] )
+        if permutation[p] < 0 and p < n - 1 and abs( permutation[p + 1] ) < abs( permutation[p] ):
+            ( permutation[p], permutation[p + 1] ) = ( permutation[p + 1], permutation[p] )
             return ( True, p, p + 1 )
         #si le décalage n'est pas possible, change la direction
-        listePermutation[p] = -listePermutation[p]
+        permutation[p] = -permutation[p]
     return ( False, 0, 0 )
 
             
 def forceBrute(n):
     ''' parcourt les permutations, vérifie si elles sont solutions
     - n : taille de l'échiquier '''
-    listePermutation = []
+    listeSolution = []
+    permutation = []
     # variable testant si l'algorithme de permutations est arrivé à terme (True: l'algorithme n'est pas terminé, False: toutes les permutations ont été parcourues
     isPermutation = True
     #initialise la première permutation
-    listePermutation = np.linspace( 0, n - 1, n )
-    if test( listePermutation, n ):
-        listeSolution.append(listePermutation)
+    permutation = np.linspace( 0, n - 1, n )
+    if test( permutation, n ):
+        listeSolution.append(permutation)
     #parcourt les permutations
     while isPermutation:
         #calcul de la permutation suivante
-        resultat = nextPermutationSJT( listePermutation, n )
+        resultat = nextPermutationSJT( permutation, n )
         isPermutation = resultat[0]
         # si la permutation est solution, l'enregistre
-        if isPermutation and test( listePermutation, n ):
-                listeSolution.append(listePermutation)
-    #affiche le nombre de solutions
+        if isPermutation and test( permutation, n ):
+            permutationPositive = [ abs(x) for x in permutation ]
+            listeSolution.append(permutationPositive)
     return(listeSolution)
     
     
-def miseAJourCollisions( listePermutation, i, j, matriceCollision, n ):
+def miseAJourCollisions( permutation, i, j, matriceCollision, n ):
     ''' met à jour la matrice de collision M selon L sachant que les dames i et j ont été permutées
-    - listePermutation : permutation actuelle
+    - permutation : permutation actuelle
     - i : première dame déplacée
     - j : deuxième dame déplacée
     - matriceCollision : matrice de transposition
@@ -171,14 +152,14 @@ def miseAJourCollisions( listePermutation, i, j, matriceCollision, n ):
     #True si il n'y a pas de collisions sur les deux dames interverties, False sinon        
     isCollision = True
     for k in range(n):
-        if  k != i  and abs( k - i ) == abs( abs( listePermutation[k] )-abs( listePermutation[i] ) ) :
+        if  k != i  and abs( k - i ) == abs( abs( permutation[k] )-abs( permutation[i] ) ) :
             isCollision = False
             matriceCollision[k][i] = 1
             matriceCollision[i][k] = 1
         else:
             matriceCollision[k][i] = 0
             matriceCollision[i][k] = 0
-        if  k != j  and abs( k - j ) == abs( abs( listePermutation[k] )-abs( listePermutation[j] ) ) :
+        if  k != j  and abs( k - j ) == abs( abs( permutation[k] )-abs( permutation[j] ) ) :
             isCollision = False
             matriceCollision[k][j] = 1
             matriceCollision[j][k] = 1
@@ -187,7 +168,7 @@ def miseAJourCollisions( listePermutation, i, j, matriceCollision, n ):
             matriceCollision[j][k] = 0
     if isCollision:
         #s'il n'y a pas de nouvelles collisions, teste toute la matrice
-        isCollision = test( listePermutation, n )
+        isCollision = test( permutation, n )
     return isCollision
             
     
@@ -201,17 +182,18 @@ def forceBruteAmelioree( n ):
     #matrice de coillision : la i-ème ligne ou colonne correspond à la i-ème dame. A_ij = 1 si les dames i et j sont en collison, 0 sinon.
     matriceCollision = np.ones( ( n, n ) )
     #initialise la première permutation
-    listePermutation = np.linspace( 0, n - 1, n )
-    if test( listePermutation, n ):
-        listeSolution.append(listePermutation)
+    permutation = np.linspace( 0, n - 1, n )
+    if test( permutation, n ):
+        listeSolution.append(permutation)
     #parcourt les permutations
     while isPermutation:
         #calcul de la permutation suivante
-        resultat = nextPermutationSJT( listePermutation, n )
+        resultat = nextPermutationSJT( permutation, n )
         isPermutation = resultat[0]
         # si la permutation est solution, l'enregistre
-        if miseAJourCollisions( listePermutation, resultat[1], resultat[2], matriceCollision, n ):
-            listeSolution.append(listePermutation)
-    #affiche le nombre de solutions
+        if miseAJourCollisions( permutation, resultat[1], resultat[2], matriceCollision, n ):
+            permutationPositive = [ abs(x) for x in permutation ]
+            listeSolution.append(permutationPositive)
     return(listeSolution)
                 
+print(forceBruteAmelioree(5))
